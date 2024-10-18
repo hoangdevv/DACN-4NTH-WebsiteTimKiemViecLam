98a91af85049ef10398632bcefa4c46291b47c8f
Hoangdev <nvhhoang286@gmail.com>
2024-10-18 19:48:00 +0700

    created a project

---
+++ src/main/java/com/codeforworks/NTH_WorkFinder/NthWorkFinderApplication.java
@@ -0,0 +1,13 @@
+package com.codeforworks.NTH_WorkFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NthWorkFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NthWorkFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeforworks/NTH_WorkFinder/NthWorkFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeforworks.NTH_WorkFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NthWorkFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
